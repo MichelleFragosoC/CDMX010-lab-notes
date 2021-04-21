34025bf49a91ecf7f57528f86f8f5f6b23587b5e
MichelleFragosoC <michellefragosoc@outlook.com>
2021-04-21 09:07:38 -0500

    First commit

---
+++ src/components/noteForm/NoteForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './NoteForm.css';
+
+class NoteForm extends React.Component{
+
+    constructor() {
+        super();
+        this.addNote = this.addNote.bind(this);
+    }
+
+    addNote(){
+        this.props.addNote(this.textInput.value);
+        this.textInput.value = '';
+        this.textInput.focus(); 
+    }
+
+    render(){
+        return(
+            <div className="NoteForm">
+            <input
+            ref={input => {this.textInput = input;}}
+            placeholder="Escribe una nota" 
+            type="text"/>
+
+        <button
+            onClick={this.addNote}
+        >
+            Add Note
+        </button>
+    </div>
+        )
+    }
+}
+
+export default NoteForm;
